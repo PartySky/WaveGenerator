ec41b602b3553a718d3297eb249f5ceb97e338a1
Yuri Partitski <PartySky@users.noreply.github.com>
2017-11-12 20:23:57 +0200

    separated-two-methods

--- WaveChart/Program.cs
+++ WaveChart/Program.cs
@@ -6,22 +6,77 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 
+
 namespace WaveChart
 {
     public class Program
     {
-		public static void Main(string[] args)
-		{
-			var host = new WebHostBuilder()
-				.UseKestrel()
-				.UseContentRoot(Directory.GetCurrentDirectory())
-				.UseIISIntegration()
-				.UseStartup<Startup>()
-				.UseApplicationInsights()
-				.Build();
+        private static byte[] myWaveData;
 
-			Console.WriteLine("qwe");
-            host.Run();
-		}
-	}
+        // Sample rate (Or number of samples in one second)
+        private const int SAMPLE_FREQUENCY = 44100;
+        // 60 seconds or 1 minute of audio
+        private const int AUDIO_LENGTH_IN_SECONDS = 1;
+
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            int generationMethod = 1;
+            switch (generationMethod)
+            {
+                case 1:
+                    
+                    List<Byte> tempBytes = new List<byte>();
+
+                    WaveHeaderA header = new WaveHeaderA();
+                    FormatChunk format = new FormatChunk();
+                    DataChunk data = new DataChunk();
+
+                    // Create 1 second of tone at 697Hz
+					SineGenerator leftData = new SineGenerator(697.0f, SAMPLE_FREQUENCY, AUDIO_LENGTH_IN_SECONDS);
+                    // Create 1 second of tone at 1209Hz
+                    SineGenerator rightData = new SineGenerator(1209.0f,
+                       SAMPLE_FREQUENCY, AUDIO_LENGTH_IN_SECONDS);
+
+                    data.AddSampleData(leftData.Data, rightData.Data);
+
+                    header.FileLength += format.Length() + data.Length();
+
+                    tempBytes.AddRange(header.GetBytes());
+                    tempBytes.AddRange(format.GetBytes());
+                    tempBytes.AddRange(data.GetBytes());
+
+                    myWaveData = tempBytes.ToArray();
+
+                    //myWaveData = File.ReadAllBytes("./ForTests/load.wav");
+
+                    File.WriteAllBytes("./ForTests/test2.wav", myWaveData);
+                    File.WriteAllBytes("./ForTests/data.tsv", myWaveData);
+                    break;
+
+                case 2:
+
+                    string filePath = @".\ForTests\test2.wav";
+                    //WaveGenerator wave = new WaveGenerator(WaveExampleType.ExampleSineWave);
+                    WaveGenerator wave = new WaveGenerator(WaveChart.WaveExampleType.ExampleSineWave);
+                    wave.Save(filePath);
+
+                    break;
+                default:
+                    break;
+            }
+
+
+            Console.WriteLine();
+
+            //host.Run();
+        }
+    }
 }
