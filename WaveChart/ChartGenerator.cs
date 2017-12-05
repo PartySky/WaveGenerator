2b0501aad504d917395ed3181162daf7e230f3d9
Yuri Partitski <PartySky@users.noreply.github.com>
2017-12-05 22:05:20 +0200

    Added ChartGenerator class
    * ChartGenerator reads the wave and write makes data object for chart

---
+++ WaveChart/ChartGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace WaveChart
+{
+    public class ChartGenerator
+    {
+        WaveHeader header;
+        WaveFormatChunk format;
+        WaveDataChunk data;
+
+        public ChartGenerator()
+        {
+            header = new WaveHeader();
+            format = new WaveFormatChunk();
+            data = new WaveDataChunk();
+        }
+
+        byte[] sRiffType = new byte[40];
+
+        public void Read(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("File doesn't exist, path: {0}", filePath);
+                throw new ArgumentNullException(filePath);
+                //return;
+            }
+
+            FileStream fileStream = new FileStream(filePath, FileMode.Open);
+            BinaryReader reader = new BinaryReader(fileStream);
+
+            // Write the header
+            //header.sGroupID = reader.ReadString(); // returns more long string then sGroupID 
+            header.sGroupID = new string(reader.ReadChars(4));
+            //header.dwFileLength = (uint)reader.ReadInt16();
+            //header.sRiffType = new string(reader.ReadChars(4));
+
+            // second way
+            fileStream.Read(sRiffType, 0, 14);
+            Encoding.ASCII.GetString(sRiffType);
+
+            Console.WriteLine("sGroupID {0}", header.sGroupID);
+            Console.WriteLine("dwFileLength {0}", header.dwFileLength);
+            //Console.WriteLine("sRiffType {0}", header.sRiffType);
+
+            reader.Dispose();
+            fileStream.Dispose();
+        }
+    }
+}
